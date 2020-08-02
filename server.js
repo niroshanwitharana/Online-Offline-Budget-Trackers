e5011517df11e355d8d7bfbdca441ef722766c98
niroshanwitharana <nrosh_uoc@yahoo.com>
2020-08-02 12:42:58 +0100

    serviceworker updated

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
+app.use(compression({ filter: shouldCompress }))
+ 
+function shouldCompress (req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false
+  }
+ 
+  // fallback to standard filter function
+  return compression.filter(req, res)
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
